0bf03142a1a01790d79004d698e8662784ff10d5
gogo <eric27202660@gmail.com>
2021-12-05 02:11:12 +0800

    Initial Commit

---
+++ ios_hw3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios_hw3
+//
+//  Created by 官鼎鈞 on 2021/12/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios_hw3/ios_hw3App.swift
@@ -0,0 +1,17 @@
+//
+//  ios_hw3App.swift
+//  ios_hw3
+//
+//  Created by 官鼎鈞 on 2021/12/5.
+//
+
+import SwiftUI
+
+@main
+struct ios_hw3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
